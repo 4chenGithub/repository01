7dbe40c3731b057757d9eb7dc9074558e35241ff
admin <1007942602@qq.com>
2019-10-12 14:53:35 +0800

    添加注释

--- a.java
+++ a.java
@@ -1,6 +1,6 @@
 public class main{
 	public static void main(String[] args){
 		System.out.println("Hello World");
-		System.out.println("Hello World");
+		System.out.println("Hello World");//Êä³öÔ¤Ô¼
 	}
 }
